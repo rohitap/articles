acef03b1c0644b1782353e1910b2d5cca1256b17
Rohit <rohitapps994@gmail.com>
2021-07-05 23:30:17 +0530

    Added axios common file

---
+++ utils/axios.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+class ApiClient {
+  constructor(axiosInst) {
+    this.axios = axiosInst
+  }
+
+  makeRequest = (url, method, data = {}) => this.axios({
+    url,
+    method,
+    data,
+  })
+
+  getRequest = (url, config) => this.makeRequest(url, 'GET', config)
+
+  postRequest = (url, config) => this.makeRequest(url, 'POST', config)
+
+  // getArticles = () => this.getRequest('/records')
+  
+  addArticles = (data) => this.postRequest('/articles/', data)
+}
+
+const axiosInst = axios.create({
+  baseURL:'http://localhost:3000/api'
+})
+
+
+export default new ApiClient(axiosInst)
